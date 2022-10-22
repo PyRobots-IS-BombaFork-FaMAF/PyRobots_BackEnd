e2acec006f2d5c769a18cb343484c775827cf606
jaelkohler <jaelkohler@gmail.com>
2022-10-22 18:19:28 -0300

    implementacion de la funcion loadRobots, falta implementar runSimulation

--- app/core/game/game.py
+++ app/core/game/game.py
@@ -1,20 +1,17 @@
-from app.core.game.robot import *
+from app.core.game.robot import Robot
 from types import ModuleType
 
 class game():
 
-    def __init__(self, quantityRound):
+    def __init__(self, quantityRound: int, robots: 'list[Robot]'):
         self.round = 1
         if(quantityRound < 10000):
             self.quantityRound = quantityRound
         else:
             self.quantityRound = 10000
         self.listRobot = []
-        self.listMove = []
+        self.listMove = robots
         self.robotPosition = dict() 
-
-    def add(self, robotList):
-        self.listRobot.append(robotList)
 
     def advance_round(self):
         if(self.round <= self.quantityRound):
@@ -40,15 +37,10 @@ class game():
             self.advance_round()
 
 
-
-
-
-def runSimulation(pathsToRobots: 'list[str]') -> 'list[dict[str, any]]':
+def loadRobots(pathsToRobots: 'list[str]') -> 'list[Robot]':
     """
-    Run a simulation with the robots on the given paths
-    Paths are in python format (e.g. 'app.robot_code.robot1')
+    Load the robots from the given paths
     """
-
     robotsModules: 'list[ModuleType]' = list(map((lambda path: __import__(path, fromlist='.'.join(path.split('.')[1:]))), pathsToRobots))
     robotsNames: 'list[str]' = list(map((lambda path: path.split('.')[-1]), pathsToRobots))
 
@@ -58,9 +50,17 @@ def runSimulation(pathsToRobots: 'list[str]') -> 'list[dict[str, any]]':
 
     robots: 'list[Robot]' = list(map(lambda robotClass: robotClass(), robotsClasses))
 
+    return robots
+
+
+def runSimulation(pathsToRobots: 'list[str]') -> 'list[dict[str, any]]':
+    """
+    Run a simulation with the robots on the given paths
+    Paths are in python format (e.g. 'app.robot_code.robot1')
+    """
+    
 
     pass
-
 
 
 
