5171deffd7782aa5c89cc4071400e3b03cb77ca7
Fuentes Tiffany <tiffanybricett1996@gmail.com>
2022-10-30 22:31:44 -0300

    PYR-74 Correccion carga de partidas desde BD

--- app/core/game/partida.py
+++ app/core/game/partida.py
@@ -21,7 +21,7 @@ class PartidaObject():
         self._min_players = min_players
         self._creator = creator
         self._players = [player_robot] if not fromdb else player_robot
-        lf._current_players = len(self._players)
+        self._current_players = len(self._players)
         self._creation_date = (datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f") 
             if not creation_date else creation_date)
         self._password = "" if not password else (hash_password(password) if not fromdb else password)
