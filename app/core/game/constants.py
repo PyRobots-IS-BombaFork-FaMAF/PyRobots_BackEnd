b2149746de9231c4e9553871fb2406b84230c8a3
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2022-11-02 20:23:53 -0300

    PYR-70 Change `missile_velocity` to `3.5`

--- app/core/game/constants.py
+++ app/core/game/constants.py
@@ -1,4 +1,4 @@
 max_velocity: float = 1      # m/round
 acceleration: float = 0.2    # m/round²  # also limit of deceleration
 board_size: int = 1000     # m
-missile_velocity: int = 350
+missile_velocity: int = 3.5
