598ac43cd8206347f30ada337ce237f26e066de2
jaelkohler <jaelkohler@gmail.com>
2022-11-02 12:17:46 -0300

    PYR-83 agregando test al endpoint simulacion

--- app/tests/test_simulacion.py
+++ app/tests/test_simulacion.py
@@ -46,6 +46,7 @@ def test_create_simulation():
     )
     assert response.status_code == 200
 
+
 def test_create_simulation_invalid_number_robots():
     response_login = client.post(
         "/token",
@@ -80,6 +81,84 @@ def test_create_simulation_invalid_number_robots():
     )
     assert response.status_code == 400
 
+def test_create_simulation_overflow_number_robots():
+    response_login = client.post(
+        "/token",
+        data={
+            "grant_type": "",
+            "username": "tiffbri",
+            "password": "Tiffanyb19!",
+            "scope": "",
+            "client_id": "",
+            "client_secret": "",
+        },
+    )
+    assert response_login.status_code == 200
+    rta: dict = response_login.json()
+    token: str = rta["access_token"]
+    token_type: str = "Bearer "
+    head: str = token_type + token
+    body = {
+            "robots": [
+                {
+                "id": 2
+                },
+                {
+                "id": 2
+                },
+                {
+                "id": 2
+                },
+                {
+                "id": 2
+                },
+                {
+                "id": 2
+                }
+            ],
+            "rounds": {
+                "rounds": 1000
+            }    
+        }
+    response = client.post(
+        "/simulation",
+        headers={"accept": "test_application/json", "Authorization": head},
+        json=body
+    )
+    assert response.status_code == 400
+
+
+def test_create_simulation_empty_number_robots():
+    response_login = client.post(
+        "/token",
+        data={
+            "grant_type": "",
+            "username": "tiffbri",
+            "password": "Tiffanyb19!",
+            "scope": "",
+            "client_id": "",
+            "client_secret": "",
+        },
+    )
+    assert response_login.status_code == 200
+    rta: dict = response_login.json()
+    token: str = rta["access_token"]
+    token_type: str = "Bearer "
+    head: str = token_type + token
+    body = {
+            "robots": [],
+            "rounds": {
+                "rounds": 1000
+            }    
+        }
+    response = client.post(
+        "/simulation",
+        headers={"accept": "test_application/json", "Authorization": head},
+        json=body
+    )
+    assert response.status_code == 400
+
+
 def test_create_simulation_invalid_number_rounds():
     response_login = client.post(
         "/token",
@@ -108,6 +187,44 @@ def test_create_simulation_invalid_number_rounds():
             ],
             "rounds": {
                 "rounds": 10001
+            }    
+        }
+    response = client.post(
+        "/simulation",
+        headers={"accept": "test_application/json", "Authorization": head},
+        json=body
+    )
+    assert response.status_code == 422
+    
+
+def test_create_simulation_negative_number_rounds():
+    response_login = client.post(
+        "/token",
+        data={
+            "grant_type": "",
+            "username": "tiffbri",
+            "password": "Tiffanyb19!",
+            "scope": "",
+            "client_id": "",
+            "client_secret": "",
+        },
+    )
+    assert response_login.status_code == 200
+    rta: dict = response_login.json()
+    token: str = rta["access_token"]
+    token_type: str = "Bearer "
+    head: str = token_type + token
+    body = {
+            "robots": [
+                {
+                "id": 2
+                },
+                {
+                "id": 3
+                }
+            ],
+            "rounds": {
+                "rounds": -1
             }    
         }
     response = client.post(
