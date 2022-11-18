db64bca7ed8be6842b2e84a7c4e18ae626fa138d
GeerJohn <germanjohn13@outlook.es>
2022-11-18 13:46:08 -0300

    PYR-91 Algunos tests mas

--- app/tests/test_user.py
+++ app/tests/test_user.py
@@ -68,5 +68,36 @@ def test_user_name_and_email():
     
 
     
+def test_username_invalid():
+    response_login = client.post(
+        "/token",
+        data = {
+            "grant_type": "",
+            "username": "tiffbri",
+            "password": "Tiffanyb19!",
+            "email": "tiffanybricett1281996@gmail.com",
+            "scope": "",
+            "client_id": "",
+            "client_secret": "",
+        },
+    )
+    assert response_login.status_code == 200
+    rta: dict = response_login.json()
+    token: str = rta["access_token"]
+    token_type: str = "Bearer "
+    head: str = token_type + token
+    body = {
+        }
+    response = client.get(
+        "/user/info",
+        headers={"accept": "test_application/json", "Authorization": head},
+        json=body
+    )
+    tmp_user = response.json()
+    assert response.status_code == 200
+    assert len(tmp_user) == 3
+    assert tmp_user["name"] != "diferentName"
+
 def test_user_avatar():
-    pass
+    pass  
+    
